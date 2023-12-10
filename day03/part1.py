1d703d38325210c59bc45b47f821c80d698d308e
Juan Carlos Zamora <juazasan@msn.com>
2023-12-10 12:08:43 +0100

    set up day03

--- day03/part1.py
+++ day03/part1.py
@@ -1,45 +1,97 @@
 """advent of code day 2 part 1"""
 
-def is_game_possible(games, cubes):
-    """Checks if the game is possible"""
-    for game in games:
-        for color in game:
-            if game[color] > cubes[color]:
-                return False
+def is_character(c):
+    """Checks if c is a character"""
+    if c == '.':
+        return False
+    if c.isdigit():
+        return False
     return True
 
-def parse_game(line):
-    """Parse a game"""
-    game_id =int(line.split(':')[0].split(' ')[1])
-    games = []
-    for game in line.split(':')[1].split(';'):
-        new_game = {
-            'red': 0,
-            'green': 0,
-            'blue': 0,
-        }
-        for cube in game.split(','):
-            new_game[cube.strip().split(' ')[1]] = int(cube.strip().split(' ')[0])
-        games.append(new_game)
-    return {
-        'id': game_id,
-        'games': games,
-    }
+def c_has_adjacent(x, y, schmtic):
+    """Checks if x,y has any adjacent characters around in schmtic"""
+    cell_has_adjacent = False
+    if 0 < x < len(schmtic[y])-1 and 0 < y < len(schmtic)-1 and \
+        any(is_character(c) for c in [schmtic[y][x-1],
+                                     schmtic[y][x+1],
+                                     schmtic[y-1][x],
+                                     schmtic[y+1][x],
+                                     schmtic[y-1][x-1],
+                                     schmtic[y-1][x+1],
+                                     schmtic[y+1][x-1],
+                                     schmtic[y+1][x+1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == 0 and y == 0 and \
+        any(is_character(c) for c in [schmtic[y][x+1],schmtic[y+1][x],schmtic[y+1][x+1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == 0 and y == len(schmtic)-1 and \
+        any(is_character(c) for c in [schmtic[y][x+1],schmtic[y-1][x],schmtic[y-1][x+1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == len(schmtic[y])-1 and y == 0 and \
+        any(is_character(c) for c in [schmtic[y][x-1],schmtic[y+1][x],schmtic[y+1][x-1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == len(schmtic[y])-1 and y == len(schmtic)-1 and \
+        any(is_character(c) for c in [schmtic[y][x-1],schmtic[y-1][x],schmtic[y-1][x-1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == 0 and 0 < y < len(schmtic)-1 and \
+        any(is_character(c) for c in [schmtic[y][x+1],
+                                      schmtic[y-1][x],
+                                      schmtic[y+1][x],
+                                      schmtic[y-1][x+1],
+                                      schmtic[y+1][x+1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and x == len(schmtic[y])-1 and 0 < y < len(schmtic)-1 and \
+        any(is_character(c) for c in [schmtic[y][x-1],
+                                      schmtic[y-1][x],
+                                      schmtic[y+1][x],
+                                      schmtic[y-1][x-1],
+                                      schmtic[y+1][x-1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and y == 0 and 0 < x < len(schmtic[y])-1 and \
+        any(is_character(c) for c in [schmtic[y][x-1],
+                                      schmtic[y][x+1],
+                                      schmtic[y+1][x],
+                                      schmtic[y+1][x-1],
+                                      schmtic[y+1][x+1]]):
+        cell_has_adjacent = True
+    if not cell_has_adjacent and y == len(schmtic)-1 and 0 < x < len(schmtic[y])-1 and \
+        any(is_character(c) for c in [schmtic[y][x-1],
+                                      schmtic[y][x+1],
+                                      schmtic[y-1][x],
+                                      schmtic[y-1][x-1],
+                                      schmtic[y-1][x+1]]):
+        cell_has_adjacent = True
+    return cell_has_adjacent
 
-def day02_part1(file_name, cubes):
-    """main function"""
+def sum_parts(schematic):
+    """sum vaid engine parts"""
     total = 0
-    with open(file_name, 'r', encoding='utf-8') as file:
-        for line in file:
-            game = parse_game(line)
-            if is_game_possible(game['games'], cubes):
-                total += game['id']
+    for y, r in enumerate(schematic):
+        is_new_number = True
+        is_valid_part = False
+        new_number = 0
+
+        for x, c in enumerate(r):
+            if not c.isdigit() and not is_new_number and is_valid_part:
+                total += new_number
+                is_new_number = True
+                is_valid_part = False
+                new_number = 0
+                continue
+            if c.isdigit() and not is_new_number:
+                new_number *= 10
+                new_number += int(c)
+            if c.isdigit() and is_new_number:
+                new_number = int(c)
+                is_new_number = False
+            if not is_valid_part:
+                is_valid_part = c_has_adjacent(x, y, schematic)
     return total
 
 if __name__ == '__main__':
-    cubes_guess = {
-        'red': 12,
-        'green': 13,
-        'blue': 14,
-    }
-    print(str(day02_part1('input', cubes_guess)))
+    source_schmtic = []
+    with open('input', 'r', encoding='utf-8') as file:
+        for line in file:
+            row = list(line)
+        source_schmtic.append(row)
+    print(str(sum_parts(source_schmtic)))
